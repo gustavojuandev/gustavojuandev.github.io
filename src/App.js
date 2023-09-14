de56674b70e27d7db2092b8337700925c392b1e4
Gustavo Juan <gustavojuandev@gmail.com>
2023-09-14 13:24:06 +0200

    Estados con useState

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 import React from "react";
 
-
 const defaultTodos = [
   { text: "Hacer la compra", completed: false },
   { text: "Escribir informe de proyecto", completed: true },
@@ -14,8 +13,8 @@ const defaultTodos = [
   { text: "Estudiar programación", completed: false },
   { text: "Llamar al médico", completed: true },
   { text: "Limpiar la casa", completed: false },
-  { text: "Preparar la cena", completed: false },
-  { text: "Leer un libro", completed: false },
+  { text: "Preparar la cena", completed: true },
+  { text: "Leer un libro", completed: true },
   { text: "Planificar vacaciones", completed: false },
   { text: "Revisar el correo electrónico", completed: true },
   { text: "Ir al gimnasio", completed: false },
@@ -23,10 +22,18 @@ const defaultTodos = [
 ];
 
 function App() {
+
+  const [searchValue, setSearchValue] = React.useState("");
+  const [todos, setTodos]  = React.useState(defaultTodos);
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const  totalTodos = todos.length;
+  
+
   return (
     <React.Fragment>
-      <TodoCounter completed={4} total={46} />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
         {defaultTodos.map((todo) => (
@@ -44,3 +51,4 @@ function App() {
 }
 
 export default App;
+
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,20 +1,17 @@
-import './TodoSearch.css';
+import React from "react";
+import "./TodoSearch.css";
 
-function TodoSearch() {
-    return  (
-      <input 
-        placeholder="Cortar cebolla"  
-        className="TodoSearch"
-        onChange={
-          (event)=>{
-              console.log(event);
-              console.log(event.target);
-              console.log(event.target.value);
-          }
-        } 
-      />
-    )
-      
+function TodoSearch({ searchValue, setSearchValue }) {
+  return (
+    <input
+      placeholder="Cortar cebolla"
+      className="TodoSearch"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
